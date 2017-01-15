d62814fd8eaa52c514c0242eda8dca25d10e7cfc
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-14 23:18:00 -0800

    changed layout constraints during app launch

--- Pedal/NoCheckupsViewController.swift
+++ Pedal/NoCheckupsViewController.swift
@@ -33,17 +33,7 @@ class NoCheckupsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func settingsButton(_ sender: UIButton) {
         self.performSegue(withIdentifier: "settings", sender: self)
